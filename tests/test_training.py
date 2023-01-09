3b6be55c9498ecb1a3a90eaa8199f23cde00f524
FredslundMagnus <s183905@student.dtu.dk>
2023-01-09 13:36:40 +0100

    f

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,2 +1,6 @@
-def test_training():
-    pass
+import pytest
+
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 54)])
+def test_training(test_input, expected):
+    assert eval(test_input) == expected
